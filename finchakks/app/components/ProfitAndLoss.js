944e7ab1764d3337d0a674fa0bc902d79f938aa0
sreenathchakkingal <sreenathchakkingal04@gmail.com>
2016-12-28 22:02:12 +0530

    diff in summary

--- finchakks/app/components/ProfitAndLoss.js
+++ finchakks/app/components/ProfitAndLoss.js
@@ -26,17 +26,25 @@ function ProfitAndLoss (props) {
             <thead>
               <tr>
                 <th>Return(%)</th>
+                <th>Prev Return(%)</th>
+                <th>Diff</th>
                 <th>Investment</th>
                 <th>Return</th>
-                <th>Return(Bank)</th>
+                <th>Prev Return</th>
                 <th>Diff</th>
+                <th>Return(Bank)</th>
+                <th>Diff compared to Bank</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>{formatter.convertToPercent(stockData.averageReturn)}</td>
+                <td>{formatter.convertToPercent(stockData.prevAverageReturn)}</td>
+                <td>{formatter.convertToPercent(stockData.diffInCurrentAndPrevAverageReturn)}</td>
                 <td>{formatter.moneyFormat(stockData.totalInvestment)}</td>
                 <td>{formatter.moneyFormat(stockData.totalReturn)}</td>
+                <td>{formatter.moneyFormat(stockData.prevTotalReturn)}</td>
+                <td>{formatter.moneyFormat(stockData.diffInCurrentAndPrevTotalReturn)}</td>
                 <td>{formatter.moneyFormat(stockData.totalReturnIfBank)}</td>
                 <td>{formatter.moneyFormat(stockData.totalReturnVsIfBank)}</td>
               </tr>
@@ -52,8 +60,12 @@ ProfitAndLoss.propTypes = {
   isLoading: PropTypes.bool.isRequired,
   stocksInfo: PropTypes.shape({
     averageReturn: PropTypes.number.isRequired,
+    prevAverageReturn: PropTypes.number.isRequired,
+    diffInCurrentAndPrevAverageReturn: PropTypes.number.isRequired,
     totalInvestment: PropTypes.number.isRequired,
     totalReturn: PropTypes.number.isRequired,
+    prevTotalReturn: PropTypes.number.isRequired,
+    diffInCurrentAndPrevTotalReturn: PropTypes.number.isRequired,
     totalInvestment: PropTypes.number.isRequired,
     totalReturnIfBank: PropTypes.number.isRequired,
     totalReturnVsIfBank: PropTypes.number.isRequired
